bfd695e0b43422ece02c65667f0f75c5761d366c
kentai1114 <cftai@connect.ust.hk>
2018-11-27 13:00:55 +0800

    Version-Of-Data-Analysis

--- development.py
+++ development.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from src.logger.logger import set_up_logger
-from src.node_relationship.node import get_all_relationship, drop_duplicate
+from src.node_relationship.node import get_all_relationship
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -20,51 +20,21 @@ plt.xlabel('Occurrence')
 plt.ylabel('No of Customer')
 plt.title('Occurrence of Customer')
 
-drop_duplicate(reading)
-
 len(set(reading.CUSTOMER_2_ID.values))
 
 id_1 = list(set(reading.CUSTOMER_2_ID.values))
-end_degree = []
+rows_data = []
+
 for i in range(len(id_1)):
-    customer_list,end = get_all_relationship(id_1[i],reading)
-    end_degree.append(end)
+    row_data = []
+    customer,end_degree,length,obtain_list = get_all_relationship(id_1[i],reading)
+    row_data.append(customer)
+    row_data.append(end_degree)
+    row_data.append(length)
+    row_data.append(obtain_list)
+    rows_data.append(row_data)
     if i % int(round(len(id_1)/20)) == 0:
-        print(i)
+            print(i)
 
-
-len(end_degree)
-
-
-
-plt.hist(end_degree, bins = 6)
-plt.xlabel('Max Degree')
-plt.ylabel('No of Customer')
-plt.title('Max Degree of Customer')
-
-customer_list_2 = get_all_relationship(73,reading)
-len(customer_list_2)
-
-
-pd.DataFrame(end_degree).to_csv('temp4.csv',index = False)
-
-import pickle
-with open("test.txt", "wb") as fp:
-    pickle.dump(end_degree, fp)
-
-from collections import Counter
-Counter(end_degree)
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/hi')
-def hello_world_hi():
-    return 'Hello World! ABC'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+df_test = pd.DataFrame(rows_data,columns = ['CUSTOMER_ID','END_DEGREE','NODE_SIZE','NODE'])
+df_test.to_csv('analysis_result.csv',index = False)
