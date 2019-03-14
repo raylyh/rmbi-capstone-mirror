32dec2f964003779b8198ff14fef994bee0a0987
kentai1114 <cftai@connect.ust.hk>
2019-03-15 01:10:20 +0800

    explain the program

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ config = yaml.load(f)
 def initialize():
     customerID = None
     if request.method == "POST":
-        customerID = request.form.get("customerID", None)
+        customerID = request.form.get("customerID", None) # get the customer id from website
     if customerID:
-        id = int(customerID)
+        id = int(customerID) # change it to int type
     else:
         id = random.randint(1, 297111)  #randomly choose between 1 to 297111 to show a graph
 
@@ -26,6 +26,8 @@ def initialize():
     nodes, links = get_node_edge(id, config)
 
     json_data = dict(nodes=nodes, links=links)
+
+    # send the data to index.html
     return render_template("index.html", customerID=customerID, data=json_data)
 
 if __name__ == '__main__':
--- src/mysql/mysql_in_python.py
+++ src/mysql/mysql_in_python.py
@@ -7,7 +7,17 @@ __name__ = "MySQL-In-Python"
 logger = set_up_logger(__name__)
 
 def connect_to_mysql(config):
+    """Function to connect mysql database
+
+    Args:
+        config (dict): the information of mysql
+
+    Returns:
+        client and cursor for mysql
+
+    """
     try:
+        # connect Mysql database using the config file
         client = pymysql.connect(
           user=config['USER'],
           password=config['PASSWORD'],
@@ -19,14 +29,26 @@ def connect_to_mysql(config):
         cursor = client.cursor()
         logger.info('Successful connection to MySQL Database')
     except Exception as e:
+        # to locate the error in connecting Mysql database
         logger.error("Failed to connect to MySQL Database - {}".format(e))
         raise ValueError("Please check your config file first")
 
+    # return cursor and client
     return cursor, client
 
 
 def createTable(cursor):
+    """Function to create temp table in MySQL
+
+    Schema in mysql database is not provided. We create some example tables as our customer relationship management database.
+
+    Args:
+        cursor: mysql cursor
+
+    """
+
     try:
+        # cursor execute a sql comment
         cursor.execute("""CREATE TABLE CustomerInfo (
             customerID BIGINT, name VARCHAR(100), age INT, gender VARCHAR(20), Address VARCHAR(200),
             primary key (customerID))""")
@@ -34,6 +56,7 @@ def createTable(cursor):
     except Exception as e:
         logger.error("CustomerInfo Table already exist - {}".format(e))
     try:
+        # cursor execute a sql comment
         cursor.execute("""CREATE TABLE CustomerRelationship (
             customerID1 BIGINT, customerID2 BIGINT, weight INT, type INT,
             foreign key (customerID1) references CustomerInfo(customerID),
@@ -44,9 +67,20 @@ def createTable(cursor):
 
 
 def insertData(cursor, client):
+    """Function to create temp data in MySQL
+
+    Customer data in mysql database is not provided. We create some example data in our customer relationship management database.
+
+    Args:
+        cursor: mysql cursor
+        client: mysql client
+    """
+
     sql = "INSERT INTO CustomerInfo (customerID, name, age, gender, address) VALUES (%s, %s, %s, %s, %s)"
     #customerID is in range of [1, 297111]
     #tuple: (customerID, name, age, gender, address)
+
+    # random initialize the customer information
     val = [(i,
         names.get_full_name(),
         random.randint(18,80),
@@ -55,6 +89,7 @@ def insertData(cursor, client):
     logger.info("Finish generating random customer info")
     try:
         cursor.executemany(sql, val)
+        # commit the changers to database, otherwise the inserted data will not really save in mysql database
         client.commit()
         logger.info("Inserted CustomerInfo rows: {}".format(cursor.rowcount))
     except Exception as e:
@@ -66,6 +101,7 @@ def insertData(cursor, client):
     val = list(table.itertuples(index=False, name=None))
     try:
         cursor.executemany(sql, val)
+        # commit the changers to database, otherwise the inserted data will not really save in mysql database
         client.commit()
         logger.info("Inserted CustomerRelationship rows: {}".format(cursor.rowcount))
     except Exception as e:
